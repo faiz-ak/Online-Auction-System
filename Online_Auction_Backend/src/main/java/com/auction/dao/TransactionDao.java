f50c3e6f01964aea3fdc13c9f104c3dde54e8b85
faizan-blip <faizanrock753@gmail.com>
2025-06-26 16:32:05 +0530

    Jdbc logic done by faizan

--- Online_Auction_Backend/src/main/java/com/auction/dao/TransactionDao.java
+++ Online_Auction_Backend/src/main/java/com/auction/dao/TransactionDao.java
@@ -1,5 +1,34 @@
-//FAIZAN
+// FAIZAN
 package com.auction.dao;
 
+import com.auction.util.DBUtil;
+import com.auction.model.RecordClass.Transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 public class TransactionDao {
+
+    public void placeTransaction(Transaction transaction) {
+
+        try {
+            Connection con = DBUtil.getConnection();
+            String sql = "INSERT INTO transactions (transaction_id, item_id, buyer_id, amount, transaction_time) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+
+            preparedStatement.setInt(1, transaction.id());
+            preparedStatement.setInt(2, transaction.itemId());
+            preparedStatement.setInt(3, transaction.buyerId());
+            preparedStatement.setDouble(4, transaction.amount());
+            preparedStatement.setTimestamp(5, Timestamp.valueOf(transaction.transactionTime()));
+
+            preparedStatement.executeUpdate();
+            System.out.println("Transaction placed for Item ID: " + transaction.itemId());
+
+        } catch (SQLException e) {
+            System.out.println("DB Error during transaction: " + e.getMessage());
+        }
+    }
 }
