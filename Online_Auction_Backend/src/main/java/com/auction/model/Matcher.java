fa2545f03189e07daeee79510c3cf8f9a4bac19d
Famiya Tasneem <famiyatasneem@gmail.com>
2025-06-26 18:28:05 +0530

    Record and matching by Famiya

--- Online_Auction_Backend/src/main/java/com/auction/model/Matcher.java
+++ Online_Auction_Backend/src/main/java/com/auction/model/Matcher.java
@@ -1,5 +1,26 @@
 //FAMIYA
+
+
 package com.auction.model;
 
+
+
 public class Matcher {
+    public static void match(Object obj) {
+        switch (obj) {
+            case RecordClass.User(int user_id, String name, String email) ->
+                    System.out.println(" USER → ID: " + user_id + " | Name: " + name + " | Email: " + email);
+
+            case RecordClass.AuctionItem(int item_id, String title, String category, double start_price) ->
+                    System.out.println(" AUCTION ITEM → ID: " + item_id + " | Title: " + title + " | Category: " + category + " | Start Price: ₹" + start_price);
+
+            case RecordClass.Bid(int bid_id, int user_id, int item_id, double amount, var bid_time) ->
+                    System.out.println(" BID → Bid ID: " + bid_id + " | User ID: " + user_id + " | Item ID: " + item_id + " | Amount: ₹" + amount + " | Time: " + bid_time);
+
+            case RecordClass.Transaction trans ->
+                    System.out.println(" TRANSACTION → Transaction ID: " + trans.transaction_id() + " | Item ID: " + trans.item_id() + " | Buyer ID: " + trans.buyer_id() + " | Amount: ₹" + trans.amount() + " | Time: " + trans.transaction_time());
+
+            default -> System.out.println(" Unknown object type.");
+        }
+    }
 }
--- Online_Auction_Backend/src/main/java/com/auction/model/RecordClass.java
+++ Online_Auction_Backend/src/main/java/com/auction/model/RecordClass.java
@@ -1,6 +1,13 @@
 //FAMIYA
 
 package com.auction.model;
+import java.time.LocalDateTime;
+
+
 
 public class RecordClass {
+ public record User(int user_id,String name,String email){}
+ public record AuctionItem(int item_id,String title,String category,double start_price){}
+ public record Bid(int bid_id,int user_id,int item_id,double amount,LocalDateTime bid_time){}
+public record  Transaction(int transaction_id,int item_id,int buyer_id,double amount,LocalDateTime transaction_time){}
 }
